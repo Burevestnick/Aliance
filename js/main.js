92e1ff65b8da568057f59ab42ea70dd89716b388
Burevestnick <bychkov1058@inbox.ru>
2024-04-07 17:10:20 +0300

    fix: icons svg sprites

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1){
     navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprites.svg#logo-dark";
   } else {
     navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprites.svg#logo-light";
+
   }
 });
 
