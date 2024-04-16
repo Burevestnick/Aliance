0915a2fb7a17f69f2ef551469d3fe62ee2b5b522
Burevestnick <bychkov1058@inbox.ru>
2024-04-16 18:36:33 +0300

    add: side-menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".navbar-logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const Menu = document.querySelector(".mobile-menu");
 window.addEventListener('scroll', () => {
   if (this.scrollY > 1){
     navbar.classList.add("navbar-light");
@@ -7,7 +9,9 @@ window.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove("navbar-light");
     logo.href.baseVal = "img/sprites.svg#logo-light";
-
   }
 });
-
+mMenuToggle.addEventListener('click', (event) => {
+  event.preventDefault();
+  Menu.classList.toggle("is-open");               
+})
