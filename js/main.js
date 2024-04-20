007683ee4b49e1317146641cbcdb44991470269f
Burevestnick <bychkov1058@inbox.ru>
2024-04-20 20:34:07 +0300

    fix: mobile menu look, header light and dark look with mobile menu open

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".navbar-logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
-const Menu = document.querySelector(".mobile-menu");
+const menu = document.querySelector(".mobile-menu");
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprites.svg#logo-dark";
+}
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprites.svg#logo-light";
+}
+const openMenu = (event) => {
+  menu.classList.add("is-open");
+  mMenuToggle.classList.add('close-menu');
+  document.body.style.overflow="hidden";
+  lightModeOn();
+}
+const closeMenu = (event) => {
+  menu.classList.remove("is-open");
+  mMenuToggle.classList.remove('close-menu');
+  document.body.style.overflow="";
+  lightModeOff();
+}
+
 window.addEventListener('scroll', () => {
-  if (this.scrollY > 1){
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprites.svg#logo-dark";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprites.svg#logo-light";
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 mMenuToggle.addEventListener('click', (event) => {
   event.preventDefault();
-  Menu.classList.toggle("is-open");               
+  menu.classList.contains('is-open') ? closeMenu() : openMenu();      
 })
