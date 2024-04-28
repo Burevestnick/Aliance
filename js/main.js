b3d797404e865b157fc1e6914abc7a345644caf6
Burevestnick <bychkov1058@inbox.ru>
2024-04-28 17:27:11 +0300

    fix: logo switch

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const navbar = document.querySelector(".navbar");
-const logo = document.querySelector(".navbar-logo-svg use");
+const logoLight = document.querySelector(".logo-light");
+const logoDark = document.querySelector(".logo-dark");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logo.href.baseVal = "img/sprites.svg#logo-dark";
+  logoDark.style.display = "block";
+  logoLight.style.display = "none";
 }
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
-  logo.href.baseVal = "img/sprites.svg#logo-light";
+  logoDark.style.display = "none";
+  logoLight.style.display = "block";
 }
 const openMenu = (event) => {
   menu.classList.add("is-open");
