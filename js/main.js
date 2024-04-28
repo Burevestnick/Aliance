562dd9dfb11c394540730bfb599f0ff0842edcf6
Burevestnick <bychkov1058@inbox.ru>
2024-04-28 19:12:16 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -37,7 +37,6 @@ mMenuToggle.addEventListener('click', (event) => {
 
 const swiper = new Swiper('.swiper', {
   speed: 400,
-  autoHeight: true,
   slidesPerView: 1,
     // Responsive breakpoints
     breakpoints: {
